5a133501df52b33e5e5b1a2822edf606fa1d5e7f
chethanram <chethanram11@gmail.com>
2020-01-26 08:54:34 +0000

    deployed font awesome and cart changes

---
+++ build/precache-manifest.abdf0ac7e829c0c846896b9ecd69dbf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d65a2eacc249663292e2336e88940e2",
+    "url": "/shopping-cart-react/static/media/instagram-icon.7d65a2ea.png"
+  },
+  {
+    "revision": "5c315458bf3c72ad4bc2",
+    "url": "/shopping-cart-react/static/js/runtime~main.5c315458.js"
+  },
+  {
+    "revision": "528f79d70688ef7c488a",
+    "url": "/shopping-cart-react/static/js/main.528f79d7.chunk.js"
+  },
+  {
+    "revision": "7400688add74ff50df0c",
+    "url": "/shopping-cart-react/static/js/1.7400688a.chunk.js"
+  },
+  {
+    "revision": "528f79d70688ef7c488a",
+    "url": "/shopping-cart-react/static/css/main.837bf5d1.chunk.css"
+  },
+  {
+    "revision": "e62469806ab585e27c7a669002e5d31a",
+    "url": "/shopping-cart-react/index.html"
+  }
+];
